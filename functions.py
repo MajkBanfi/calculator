b9f83eccb96ddf2eca36e8e569a96d82689c8b99
MajkBanfi <majk.banfi@gmail.com>
2017-10-30 18:08:23 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,30 @@
+def sum_num(first_number, second_number):
+    """
+
+    :param first_number: first number of sum
+    :param second_number: second number of sum
+    :return: sum of first and second number
+    """
+
+    c = first_number + second_number
+
+    return c
+
+def generate_list(a, b, c, d):
+    new_list = []
+    new_list.append(a)
+    new_list.append(b)
+    new_list.append(c)
+    new_list.append(d)
+
+    return new_list
+
+# x = 3
+# y = 2
+# sum_ab = sum_num(x, y)
+#
+# print(sum_ab)
+# print(sum_num(12, 31))
+# print(sum_num(13, 41))
+# print(sum_num(0, 45))
+# print(sum_num(1, 2))
